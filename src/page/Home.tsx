9d5dbae808b491115b2523f17947e49ebe1a9a64
AKLukman <abulkashemlukman@gmail.com>
2024-01-22 18:58:51 +0000

    redux setup[done]

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,3 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div className="flex gap-5">
+        <div>
+          <button
+            className="bg-green-500"
+            onClick={() => dispatch(increment())}
+          >
+            increment
+          </button>
+        </div>
+        <div>{count}</div>
+        <div>
+          <button className="bg-red-500" onClick={() => dispatch(decrement())}>
+            decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
